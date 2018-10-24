29987d9ed189db3df84570e9f066087a20867c27
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-10-24 19:09:21 +0900

    Style outer columns

--- src/SaintElmo/View.elm
+++ src/SaintElmo/View.elm
@@ -1,9 +1,13 @@
 module SaintElmo.View exposing (view)
 
 import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
 import SaintElmo.Model exposing (Model, Msg)
 
 
 view : Model -> Html Msg
 view _ =
-    div [] [ text "Hello, Elm!" ]
+    div [ class "page" ]
+        [ div [ class "navigation" ] [ text "Navigation" ]
+        , div [ class "contents" ] [ text "Contents" ]
+        ]
