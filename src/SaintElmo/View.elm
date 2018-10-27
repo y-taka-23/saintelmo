debe2d257d90d4fb02f3c2e85b46b719bbec6247
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-10-27 22:49:19 +0900

    Style the channel list

--- src/SaintElmo/View.elm
+++ src/SaintElmo/View.elm
@@ -1,6 +1,6 @@
 module SaintElmo.View exposing (view)
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, h2, li, text, ul)
 import Html.Attributes exposing (class)
 import SaintElmo.Model exposing (Model, Msg)
 
@@ -12,7 +12,20 @@ view _ =
             [ div [ class "login-user" ]
                 [ text "Login User" ]
             , div [ class "channel-list" ]
-                [ text "Channel List" ]
+                [ h2 [ class "channel-list__header" ] [ text "CHANNELS" ]
+                , ul []
+                    [ li [ class "channel-list__name" ] [ text "lorem_ipsum" ]
+                    , li [ class "channel-list__name" ] [ text "dolor" ]
+                    , li [ class "channel-list__name" ] [ text "sit_amet" ]
+                    , li [ class "channel-list__name" ] [ text "consectetur" ]
+                    , li [ class "channel-list__name--active" ] [ text "adipiscing" ]
+                    , li [ class "channel-list__name" ] [ text "elit_sed" ]
+                    , li [ class "channel-list__name" ] [ text "do_eiusmod" ]
+                    , li [ class "channel-list__name" ] [ text "tempor" ]
+                    , li [ class "channel-list__name" ] [ text "incididunt" ]
+                    , li [ class "channel-list__name" ] [ text "ut_labore" ]
+                    ]
+                ]
             ]
         , div [ class "contents" ]
             [ div [ class "channel-header" ]
